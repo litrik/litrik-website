1dc63f1e4c97aaf0fffa8eaf298d19812945e94d
Litrik De Roy <litrik@norio.be>
2019-07-03 17:30:58 +0200

    Fixed remote dir

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-rsync -avC --no-o --no-g -e 'ssh -p 22822' --delete src/ litrik@tolbrandir:/home/litrik/data/websites/com.litrik.www/www/
+rsync -avC --no-o --no-g -e 'ssh -p 22822' --delete src/ litrik@tolbrandir:/home/litrik/data/websites/litrik/www/
 
