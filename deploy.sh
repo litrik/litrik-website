f11425eb704ba2be51ce39b76200489e607f9bee
Litrik De Roy <litrik@norio.be>
2024-12-30 16:42:43 +0100

    Deploy to tolfalas

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-rsync -avC --no-o --no-g -e 'ssh -p 22822' --delete src/ litrik@tolbrandir:/home/litrik/data/websites/litrik/www/
-
+rsync -avC --no-o --no-g --delete src/ tolfalas:/home/admin/data/websites/litrik/www/
